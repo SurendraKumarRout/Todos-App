8ae01a61203ff158b232a50c59b19478ed0dc023
SurendraKumarRout <surendra.igit@gmail.com>
2022-03-20 00:36:36 +0530

    update status completed

--- src/features/todoApp/Todos.jsx
+++ src/features/todoApp/Todos.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { addTodo, removeTodo } from "./todosSlice";
+import { addTodo, removeTodo, updateTodo, updateStatus } from "./todosSlice";
 
 function Todos() {
   const [todo, setTodo] = useState("");
+
+  const inputRef = useRef( true );
+ 
+
+  const changeFocus = () => {
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  };
+ console.log("ref",inputRef)
+  const update = (id, value, x) => {
+    if (x.which === 13) {
+      dispatch(updateTodo(({ id, item: value })));
+      inputRef.current.disabled = true;
+    }
+  };
 
   const todos = useSelector((state) => state.myTodoApp.todos);
   const dispatch = useDispatch();
@@ -29,15 +44,25 @@ function Todos() {
       >
         ADD
       </button>
-{/* {console.log(todos)} */}
-      {todos.map((e) => {
-        return (
-          <p key={e.id}>
-            {e.item}{" "}
-            <button onClick={() => dispatch(removeTodo(e.id))}>DEL</button>
-          </p>
-        );
-      })}
+      {/* {console.log(todos)} */}
+      <ul>
+        {todos.map((e) => {
+          return (
+            <li key={e.id}>
+              <textarea
+                ref={inputRef}
+                disabled={inputRef}
+                defaultValue={e.item}
+                onKeyPress={(x) => update(e.id, inputRef.current.value, x)}
+              />
+
+              <button onClick={ () => changeFocus() }>EDIT</button>
+              <button onClick={() => dispatch(updateStatus(e.id))}>STATUS</button>
+              <button onClick={() => dispatch(removeTodo(e.id))}>DEL</button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
--- src/features/todoApp/todosSlice.js
+++ src/features/todoApp/todosSlice.js
@@ -15,9 +15,37 @@ export const todosSlice = createSlice( {
         removeTodo: ( state, action ) =>
         {
            state.todos = state.todos.filter((e) => e.id !== action.payload)
+        },
+        updateTodo: ( state, action ) =>
+        {
+            state =  state.todos.map( ( e ) =>
+            {
+                if ( e.id === action.payload.id )
+                {
+                    return {
+                        ...e,
+                        item: action.payload.item,
+                    };
+                }
+                return e;
+            })
+        },
+        updateStatus: ( state, action ) =>
+        {
+            state.todos = state.todos.map( ( e ) =>
+            {
+                if ( e.id === action.payload )
+                {
+                    return {
+                        ...e,
+                        status: true,
+                    }
+                }
+                return e;
+            })
         }
     }
 } )
 
-export const { addTodo, removeTodo } = todosSlice.actions;
+export const { addTodo, removeTodo, updateTodo, updateStatus } = todosSlice.actions;
 export default todosSlice.reducer;
